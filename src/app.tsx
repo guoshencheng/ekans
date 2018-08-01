ad8d870c1be1a3bbe76270ea9776d2116b5cb806
guoshencheng <648772021@qq.com>
2018-08-01 22:26:41 +0800

    add emitter

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { EventEmitter } from 'fbemitter'
 import { Model, ReduxReducer, MapModelDispatchBindedAction } from './model';
 import * as React from 'react';
 import { Middleware, Store, createStore, applyMiddleware } from 'redux';
@@ -9,7 +10,9 @@ export interface AppOptions<State> {
   model?: Model<State>;
   middlewares?: Middleware[];
 }
-export class App<State> {
+export class App<State> extends EventEmitter {
+  static FINISH_INIT = 'FINISH_INIT'
+  static BEFORE_INIT = 'BEFORE_INIT'
   $model?: Model<State>;
   $middlewares: Middleware[];
   $reduxReducer: ReduxReducer<State>;
@@ -17,6 +20,7 @@ export class App<State> {
   $actions: MapModelDispatchBindedAction<any>;
   $reducerActions: MapModelDispatchBindedAction<any>;
   constructor(opt?: AppOptions<State>) {
+    super();
     const { model, middlewares } = opt || { middlewares: [], model: undefined };
     this.middlewares(middlewares as Middleware[]);
     this.$model = model;
@@ -34,10 +38,12 @@ export class App<State> {
       console.log(`model不能为空 请先使用 app.model(model) 来设置model`)
       return;
     }
+    this.emit(App.BEFORE_INIT, this);
     this.$reduxReducer = this.$model.toReduxReducers();
     this.store = createStore(this.$reduxReducer, composeWithDevTools(applyMiddleware(...this.$middlewares)))
     this.$actions = this.$model.toReduxActions(this.store.dispatch);
     this.$reducerActions = this.$model.toReducerAction(this.store.dispatch);
+    this.emit(App.FINISH_INIT, this);
   }
   getActions = (): MapModelDispatchBindedAction<any> => {
     return this.$actions;
